312833ebfe3510aae1acdffa5b016eb63283f42a
Ga-Rasquinho <gabriel_rasquinho@outlook.com>
2023-06-29 13:07:13 -0300

    Funcionalidades: deletar tarefa e trocar de página

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React, {useState} from "react";
 import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import TaskDetails from "./components/TaskDetails";
+
 import "./App.css";
 
 const App = () => {
@@ -44,13 +48,28 @@ const App = () => {
       setTasks(newTasks);
     };
 
+    const handleTaskDeletion = (taskId) => {
+      const newTasks = tasks.filter(task => task.id != taskId)
+
+      setTasks(newTasks);
+    };
+
     return(
-      <> 
+        <Router>
           <div className="container">
-            <AddTask handleTaskAddition={handleTaskAddition}/>
-            <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+            <Header/>
+            <Route path="/" exact render={() => (
+              <>
+                <AddTask handleTaskAddition={handleTaskAddition}/>
+                <Tasks 
+                tasks={tasks} 
+                handleTaskClick={handleTaskClick}
+                handleTaskDeletion={handleTaskDeletion}/>
+              </>
+            )}/>
+            <Route path="/:taskTitle" exact component={TaskDetails}/>
           </div>
-      </>
+        </Router>  
     );
 };
 
---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from './Button';
+import { useHistory, useParams } from "react-router-dom";
+
+import "./TaskDetails.css"
+const TaskDetails = () => {
+    const params = useParams();
+    const history = useHistory();
+
+    const handleBackButtonClick = () => {
+        history.goBack();
+    }
+    return (  
+        <>
+            <div className="back-button-container">
+                <Button onClick={handleBackButtonClick}>Voltar</Button>
+            </div>
+            <div className='task-details-container'>
+                <h2>{params.taskTitle}</h2>
+                <p>Preenchimento Preenchimento Preenchimento Preenchimento Preenchimento Preenchimento</p>
+            </div>
+        </>
+    );
+};
+ 
+export default TaskDetails;
